05f670eaae89ba9475bf7cb72fc9bcced708a04f
Pieter Belmans <pieterbelmans@gmail.com>
2013-12-16 14:51:39 +0100

    Started use of layout in the actual output, and generating series

--- php/pages/operad.php
+++ php/pages/operad.php
@@ -3,6 +3,27 @@
 require_once("php/page.php");
 require_once("php/general.php");
 
+function getOperad($key) {
+  global $database;
+
+  $sql = $database->prepare("SELECT key, name, notation, dual, series FROM operads WHERE key = :key");
+  $sql->bindParam(":key", $key);
+
+  if ($sql->execute())
+    return $sql->fetch();
+}
+
+function getPropertiesOfOperad($key) {
+  global $database;
+
+  $sql = $database->prepare("SELECT key, name FROM operad_property WHERE key = :key");
+  $sql->bindParam(":key", $key);
+
+  if ($sql->execute())
+    return $sql->fetchAll();
+}
+
+
 class OperadPage extends Page {
   private $operad;
   private $properties;
@@ -10,23 +31,22 @@ class OperadPage extends Page {
   public function __construct($database, $key) {
     $this->db = $database;
 
-    $sql = $this->db->prepare("SELECT key, name, notation, dual FROM operads WHERE key = :key");
-    $sql->bindParam(":key", $key);
+    $this->operad = getOperad($key);
+    $this->properties = getPropertiesOfOperad($key);
+  }
 
-    if ($sql->execute())
-      $this->operad = $sql->fetch();
+  public function getHead() {
+    $value = "";
 
-    $sql = $this->db->prepare("SELECT key, name FROM operad_property WHERE key = :key");
-    $sql->bindParam(":key", $key);
+    $value .= "<link type='text/css' rel='stylesheet' href='" . href("css/operad.css") . "'>";
 
-    if ($sql->execute())
-      $this->properties = $sql->fetchAll();
+    return $value;
   }
   
   public function getMain() {
     $value = "";
 
-    $value .= "<dl>";
+    $value .= "<dl class='operad'>";
 
     $value .= "<dt>Name";
     $value .= "<dd class='name'>" . $this->operad["name"];
@@ -42,12 +62,18 @@ class OperadPage extends Page {
     //
     // TODO Sym_n-representation
     // TODO dimension of the representation (if available, the first 7 dimensions are listed)
-    // TODO generating series
+
+    $value .= "<dt>Generating series";
+    if ($this->operad["series"] != "")
+      $value .= "<dd>" . $this->operad["series"];
+    else
+      $value .= "<dd>not known";
     
     // TODO is the Koszul dual always known?
     // if not: add a check whether the field is non-empty
     $value .= "<dt>Koszul dual";
-    $value .= "<dd class='koszul-dual'><a href='" . href("operads/" . $this->operad["dual"]) . "'>" . $this->operad["dual"] . "</a>";
+    $dual = getOperad($this->operad["dual"]);
+    $value .= "<dd class='koszul-dual'><a href='" . href("operads/" . $dual["key"]) . "'>$" . $dual["notation"] . "$</a>";
 
     //
     // TODO chain complex
@@ -76,12 +102,7 @@ class OperadPage extends Page {
 
     return $value;
   }
-  public function getSidebar() {
-    $value = "";
 
-
-    return $value;
-  }
   public function getTitle() {
     return " &mdash; Operad: " . $this->operad["name"];
   }
