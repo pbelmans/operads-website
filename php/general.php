aef1448225912de6722f63becb14f516a439f473
Pieter Belmans <pieterbelmans@gmail.com>
2013-12-16 14:50:35 +0100

    Removed silly code in php/general.php 

--- php/general.php
+++ php/general.php
@@ -5,16 +5,5 @@ function href($path) {
   return $config["directory"] . "/" . $path;
 }
 
-function getOperads() {
-  global $database;
 
-  $sql = $database->prepare('SELECT tag, label, file, chapter_page, book_page, book_id, value, name, type, position FROM tags WHERE tag = :tag');
-  $sql->bindParam(':tag', $tag);
-
-  if ($sql->execute()) {
-    // return first (= only) row of the result
-    while ($row = $sql->fetch()) return $row;
-  }
-  return null;
-
-}
+?>
