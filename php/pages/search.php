e3d2ccb290ef3fbd3de5b775b3ff7d2d5ffe6ecc
Pieter Belmans <pieterbelmans@gmail.com>
2013-12-20 16:44:40 +0100

    First draft of search layout

--- php/pages/search.php
+++ php/pages/search.php
@@ -7,6 +7,14 @@ class SearchPage extends Page {
   public function __construct($database) {
     $this->db = $database;
   }
+
+  public function getHead() {
+    $value = "";
+
+    $value .= "<link type='text/css' rel='stylesheet' href='" . href("css/search.css") . "'>";
+
+    return $value;
+  }
   
   public function getMain() {
     $value = "";
@@ -19,6 +27,41 @@ class SearchPage extends Page {
     $value .= "<li>search a la OEIS";
     $value .= "<li>... ?";
     $value .= "</ol>";
+
+    $value .= "<hr>";
+
+    $value .= "
+    <form>
+      <fieldset id='plaintext'>
+        <legend>Search plaintext</legend>
+        Search through some or all of the fields that can contain plaintext.
+        <br>
+        <label class='block' for='plaintext-input'>Text</label><input type='search' id='plaintext-input' name='plaintext-input'>
+        <br>
+        <span class='block'>Fields</span>
+        <label class='inline'><input type='checkbox'> Comment</label>
+        <label class='inline'><input type='checkbox'> Alternative</label>
+        <label class='inline'><input type='checkbox'> Free algebra</label>
+        <label class='inline'><input type='checkbox'> Comment</label>
+        <br>
+        <label class='inline'><input type='checkbox'> Operations</label>
+        <label class='inline'><input type='checkbox'> Symmetry</label>
+        <label class='inline'><input type='checkbox'> Relations</label>
+        <label class='inline'><input type='checkbox'> Comment</label>
+        <br style='clear: left;'>
+        <button type='submit'>Search</button>
+      </fieldset>
+
+      <fieldset id='dimensions'>
+        <legend>Search by dimensions</legend>
+        Enter a list of integers, and check whether it occurs as the list of dimensions of an operad.
+        <p>If you search locally only a naive algorithm is provided. You can also use the full power of the Online Encyclopedia of Integer Sequences, but then you will leave the site.
+        <br>
+        <label class='block' for='dimensions-input'>Dimensions</label><input type='search' name='dimensions-input'>
+        <br>
+        <button type='submit'>Search</button><br><button type='submit'>Search on OEIS.org</button>
+      </fieldset>
+    </form>";
 
     return $value;
   }
