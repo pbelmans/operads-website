bf01feb33f5a3c7c1e93a7b284e1143a01509ee1
Pieter Belmans <pieterbelmans@gmail.com>
2013-12-25 12:11:45 +0100

    Improved operad collapsing

--- js/collapse.js
+++ js/collapse.js
@@ -1,35 +1,54 @@
 $(document).ready(function() {
   // collapse all operads
   $("dl.operad dt:nth-child(n+5), dl.operad dd:nth-child(n+5)").hide();
+  
   // clicking the operad will toggle its information
-  $("dl.operad").click(function() {
-    // if the clicked element is a link we don't toggle because that induces annoying flickering
-    if (!$(event.target).is("a")) {
+  $("dl.operad").click(function(e) {
+    // if the click was on the left part of the DL we don't trigger
+    if (e.pageX > $(this).offset().left + 0.9 * $(this).width()) {
       $("dt:nth-child(n+5), dd:nth-child(n+5)", this).toggle();
       toggleText(this);
     }
   });
 
+  function indicatorHandler(e) {
+    dl = $(e.delegateTarget);
+
+    if (e.pageX > dl.offset().left + 0.9 * dl.width())
+      showIndicator(dl);
+    else
+      hideIndicator(dl);
+  }
+
+  function showIndicator(dl) {
+    toggleText(dl);
+    $("p.toggle", dl).show();
+    dl.css("cursor", "pointer");
+  }
+
+  function hideIndicator(dl) {
+    $("p.toggle", dl).hide();
+    $(dl).css("cursor", "auto");
+  }
+
   // add a visual clue that things will change
   $("dl.operad").append("<p class='toggle'>");
   $("dl.operad").hover(
-    function() {
-      toggleText(this);
-      $("p.toggle", this).show();
-      $(this).css("cursor", "pointer");
+    function(e) { 
+      $(e.delegateTarget).mousemove(indicatorHandler);
     },
-    function() {
-      $("p.toggle", this).hide();
-      $(this).css("cursor", "auto");
+    function(e) {
+      $(e.delegateTarget).unbind("mousemove");
+      hideIndicator(e.delegateTarget);
     }
   );
 
   // change the toggle symbol according to the visibility of the operad
   function toggleText(dl) {
     if ($("dt:nth-child(n+5), dd:nth-child(n+5)", dl).is(":hidden"))
-      $("p.toggle", dl).text(">>"); // TODO some nicer symbol
+      $("p.toggle", dl).html("&raquo;");
     else
-      $("p.toggle", dl).text("<<"); // TODO some nicer symbol
+      $("p.toggle", dl).html("&laquo;");
   };
 
 });
