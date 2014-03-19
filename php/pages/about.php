6421f9d45bdf6e5d16d9f17a024d4891fe8585cf
Pieter Belmans <pieterbelmans@gmail.com>
2014-03-19 21:04:03 +0100

    Changed the about page a little

--- php/pages/about.php
+++ php/pages/about.php
@@ -14,14 +14,16 @@ class AboutPage extends Page {
 
     $value .= "<h2>About</h2>";
     $value .= "<p>Some nice description...";
+    $value .= " <em>references to Bruno Vallette, Pieter Belmans, Jean-Louis Loday, ...</em>";
 
     $value .= "<h2>Statistics</h2>";
-    $value .= "<p>There are";
+    $value .= "<p>There are currently";
     $value .= "<ul>"; // TODO link these to the corresponding pages
-    $value .= "<li>" . getNumberOfOperads() . " operads";
-    $value .= "<li>" . getNumberOfReferences() . " references";
-    $value .= "<li>" . getNumberOfProperties() . " properties";
+    $value .= "<li><a href='" . href("operads") . "'>" . getNumberOfOperads() . " operads</a>";
+    $value .= "<li><a href='" . href("properties") . "'>" . getNumberOfProperties() . " properties</a>";
+    $value .= "<li><a href='" . href("references") . "'>" . getNumberOfReferences() . " references</a>";
     $value .= "</ul>";
+    $value .= "in the Encycloperad.";
 
     return $value;
   }
